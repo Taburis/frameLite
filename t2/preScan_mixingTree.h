6b296fae6d193cbc4955788fc4fec208d7f62f9f
Xiao Wang <xwang241@uic.edu>
2019-09-17 19:03:38 -0400

    add test scan macros

--- t2/preScan_mixingTree.h
+++ t2/preScan_mixingTree.h
@@ -3,6 +3,7 @@
 #define  PRESCAN_MIXINGTREE_H
 #include "rootEDM.cc"
 #include "histManager.h"
+#include "/home/wang4187/frameLite/crab/lib/configReader.h"
 
 class preScan_mixingTree : public rootEDMProducer{
 	public :
@@ -23,14 +24,24 @@ class preScan_mixingTree : public rootEDMProducer{
 //		TArrayF *pThatInterval, *pThatEntries;
 		ParaSet *ps;
 		bool (*evtCut)(float) = 0;
+		bool testWeight=0, isdata=0;
+		int HBHE, pvfilter;
+		configReader* cr= 0 ;
 };
 
 void preScan_mixingTree::analyze(){
 	if(evtCut(vz)) return 0;
 	float weight = 1;
-	hpthat->Fill(pthat, weight);
+	if(isdata){
+		if(HBHE == 0 || pvfilter == 0) return 0;
+	} else if( testWeight){
+		weight = (cr->pThatWeight(pthat))*(cr->vzWeight(vz));	
+	}
 	hvz->Fill(vz, weight);
-	hpthatStat->Fill(pthat, weight);
+	if(!isdata){
+		hpthat->Fill(pthat, weight);
+		hpthatStat->Fill(pthat, weight);
+	}
 	for(int i =0 ;i<reco_jtpt->size(); ++i){	
 		hrecojtpt->Fill(reco_jtpt->at(i), weight);
 	}	
@@ -38,7 +49,15 @@ void preScan_mixingTree::analyze(){
 
 void preScan_mixingTree::beginJob(){
 	t = handle("mixing_tree");
-	t->SetBranchAddress("pthat", &pthat);
+	if(testWeight){
+		if(cr == 0)  std::cout<<"Please load the config.root for testing!"<<std::endl;	
+	}
+	if(isdata){
+		t->SetBranchAddress("HBHENoiseFilterResultRun2Loose", &HBHE);
+		t->SetBranchAddress("pPAprimaryVertexFilter", &pvfilter);
+	}else {
+		t->SetBranchAddress("pthat", &pthat);
+	}
 	t->SetBranchAddress("vz", &vz);
 	//t->SetBranchAddress("genpt", &gen_jtpt);
 	t->SetBranchAddress("pf_jtpt", &reco_jtpt);
